ee5a5668dfbb157e086a5ebfb7365a9d0f9064a6
Diego <arnes.diego@gmail.com>
2019-04-19 12:30:50 +0900

    Add files via upload

---
+++ result.php
@@ -0,0 +1,304 @@
+<?php
+$result = array
+(
+array(1,"アクティブ・スマートタイプ","何事にも意欲的。周囲の人を引っ張る強い行動力や向上心があり、リーダーになることが多い。明るくて面倒見のよい性格で、上下分け隔てなくいろんな人から好かれる。目標に忠実で、最短距離を目指すため、周囲の都合を考えず、自分のペースで物事を動かそうとするので、周りから「ついていけない」「○○さんは優秀だから、できない人の気持ちがわからない」と思われたりもする。", ""),
+array(2,"サイレントマジョリティタイプ","「これがしたい」「あれがほしい」という欲求が希薄。大勢といるよりも、一人でマイペースに行動することを好む。波風を立てたり、面倒に巻き込まれるのが苦手。他人に無関心というより、自分の世界に踏み込まれたくないという思いが強い。周囲の人からは「おとなしい人」と思われがち。常に空気を読んで、トラブルを避けるように行動するので、人から嫌われることは少ない。", ""),
+array(3,"ポジティブ・楽天家タイプ","常にポジティブで、明るく社交的で友だちが多い。ノリが良く、フットワークも軽く、行動力がある。深く悩まず、シンプルに物事を考えるので意思決定が早い。興味の対象が幅広く、１つのことに没頭するのは苦手で、常にいろんなことに手を出したり、首をつっこんでいたい。気が多く、予定を詰め込みすぎるので、人との約束や、課題などをついつい忘れてしまうことがある。", ""),
+array(4,"頑固・カタブツタイプ","曲がったことが大嫌いで、常識を重んじる。よく言えば真面目。悪く言えば頑固。周囲から「融通が利かない」と思われがち。自己が確立されているので、周囲の評価は気にならない。こだわりが強く、視野が狭いので、イヤなものはイヤだとはっきり拒絶する。人の感情や空気を読むのが苦手なので、周囲との衝突が多い。", ""),
+array(5,"豪快・アグレッシブタイプ","ノリが良く、楽しいことや自分の欲求に忠実で、それを満たすための労力は惜しまず、あらゆる手段を模索し、努力する。器用で、要領がよいので何事もそつなくこなせる。極端な性格なので、周囲の都合を考えず、自分のペースで人を振り回したり、思ったことはズバズバと口にしたりするので、周りから「ワガママ」「ついていけない」と思われたりする。目上の人からはその実行力を評価されることが多い。", ""),
+array(6,"優しい・おっとりタイプ","優しくて穏やかで、その場の空気を読むのがとても上手い。自己主張は控えめで、気配り上手なので、どんな場面においても人から好かれる。「他人に迷惑をかけてはいけない」という意識が強いため、責任感が強く、真面目。気遣いをし過ぎたり、自分の行動に対する反省が多いなど、自己評価が低いので、一人の時は悩んだりして、ストレスがたまりがちなところもある。", ""),
+array(7,"マイペース・引っ込み思案タイプ","無口でおとなしい。物事を論理的に考えるのが得意で、自分の主張をしっかりと持っており、何かやりたいことがあっても、「失敗したら」との妄想が先だって、なかなか行動に移れず、チャンスを逃すこともある。地道にコツコツと物事を取り組むことが得意。要所要所で鋭い意見を差し込んだりする。非常に真面目で慎重。ただ、慎重過ぎるがゆえ、ネガティブ思考に陥りがち。", ""),
+array(8,"合理主義・クレバータイプ","非常に論理的で、どんなときでも冷静沈着。トラブルが起きても動じず、淡々と対処できる。器用で、要領がよく、自分の長所・短所を理解し尽しているので、無謀な挑戦はしない。自分に自信を持っているので、初対面の人にも「デキる人だな」と思わせるオーラを漂わせている。優秀がゆえに、効率ばかりを重視して、人の感情に対して鈍感で、「冷たい人」と思われることがある。vo", ""),
+array(9,"直感・ひらめきタイプ","感性が豊かで、直観力に長けている。何事も思い立ったら、すぐ行動するバイタリティがある。後先を考えたりせず、常識にとらわれない発想や行動で、周囲を驚かせることも多い天才肌。論理的に物事を考えるのが苦手。プレッシャーに弱く、精神的にムラがあるので、一定のパフォーマンスを維持できないので、周囲の期待を裏切ってしまったりすることがある。", "")
+);
+?>
+<?php
+switch ($_POST["score_level"] ) {
+  case 12211:
+  case 12221:
+  case 12231:
+  case 12311:
+  case 12321:
+  case 12331:
+  case 13211:
+  case 13221:
+  case 13231:
+  case 13311:
+  case 13321:
+  case 13331:
+  case 22211:
+  case 22221:
+  case 22231:
+  case 22311:
+  case 22321:
+  case 22331:
+  case 22333:
+  case 23211:
+  case 23221:
+  case 23231:
+  case 23233:
+  case 23311:
+  case 23321:
+  case 23323:
+  case 23331:
+  case 23332:
+  case 23333:
+  case 32211:
+  case 32221:
+  case 32231:
+  case 32233:
+  case 32311:
+  case 32321:
+  case 32323:
+  case 32331:
+  case 32332:
+  case 32333:
+  case 33211:
+  case 33221:
+  case 33223:
+  case 33231:
+  case 33232:
+  case 33233:
+  case 33311:
+  case 33321:
+  case 33322:
+  case 33323:
+  case 33331:
+  case 33332:
+  case 33333: $type = $result[0][1]; $description = $result[0][2];  break;
+  case 11111:
+  case 22222:
+  case 22223:
+  case 22232:
+  case 22233:
+  case 22322:
+  case 22323:
+  case 22332:
+  case 23222:
+  case 23223:
+  case 23232:
+  case 23322:
+  case 32222:
+  case 32223:
+  case 32232:
+  case 32322:
+  case 33222: $type = $result[1][1]; $description = $result[1][2]; break;
+  case 11121:
+  case 11131:
+  case 11221:
+  case 11222:
+  case 11223:
+  case 11231:
+  case 11232:
+  case 11233:
+  case 11321:
+  case 11322:
+  case 11323:
+  case 11331:
+  case 11332:
+  case 11333:
+  case 12121:
+  case 12131:
+  case 12222:
+  case 12223:
+  case 12232:
+  case 12233:
+  case 12322:
+  case 12323:
+  case 12332:
+  case 12333:
+  case 13121:
+  case 13131:
+  case 13222:
+  case 13223:
+  case 13232:
+  case 13233:
+  case 13322:
+  case 13323:
+  case 13332:
+  case 13333: $type = $result[2][1]; $description = $result[2][2]; break;
+  case 21111:
+  case 21222:
+  case 21223:
+  case 21232:
+  case 21233:
+  case 21322:
+  case 21323:
+  case 21332:
+  case 21333:
+  case 22111:
+  case 23111:
+  case 31111:
+  case 31222:
+  case 31223:
+  case 31232:
+  case 31233:
+  case 31322:
+  case 31323:
+  case 31332:
+  case 31333:
+  case 32111:
+  case 33111: $type = $result[3][1]; $description = $result[3][2]; break;
+  case 21121:
+  case 21131:
+  case 21221:
+  case 21231:
+  case 21331:
+  case 22121:
+  case 22131:
+  case 23121:
+  case 23131:
+  case 31121:
+  case 31131:
+  case 31221:
+  case 31231:
+  case 31331:
+  case 32121:
+  case 32131:
+  case 33121:
+  case 33131: $type = $result[4][1]; $description = $result[4][2]; break;
+  case 12111:
+  case 12112:
+  case 12113:
+  case 12122:
+  case 12123:
+  case 12132:
+  case 12133:
+  case 12212:
+  case 12213:
+  case 13111:
+  case 13112:
+  case 13113:
+  case 13122:
+  case 13123:
+  case 13132:
+  case 13133:
+  case 13212:
+  case 13213:
+  case 13312:
+  case 13313:
+  case 22112:
+  case 22113:
+  case 22212:
+  case 22213:
+  case 22312:
+  case 22313:
+  case 23112:
+  case 23113:
+  case 23212:
+  case 23213:
+  case 23312:
+  case 23313:
+  case 32112:
+  case 32113:
+  case 32212:
+  case 32213:
+  case 32312:
+  case 32313:
+  case 33112:
+  case 33113:
+  case 33212:
+  case 33213:
+  case 33312:
+  case 33313: $type = $result[5][1]; $description = $result[5][2]; break;
+  case 11112:
+  case 11113:
+  case 11122:
+  case 11123:
+  case 11132:
+  case 11133:
+  case 11212:
+  case 11213:
+  case 11312:
+  case 11313:
+  case 12312:
+  case 12313:
+  case 21112:
+  case 21113:
+  case 21212:
+  case 21213:
+  case 21312:
+  case 21313:
+  case 31112:
+  case 31113:
+  case 31212:
+  case 31213:
+  case 31312:
+  case 31313: $type = $result[6][1]; $description = $result[6][2]; break;
+  case 11211:
+  case 11311:
+  case 21211:
+  case 21311:
+  case 21321:
+  case 31211:
+  case 31311:
+  case 31321: $type = $result[7][1]; $description = $result[7][2]; break;
+  case 21122:
+  case 21123:
+  case 21132:
+  case 21133:
+  case 22122:
+  case 22123:
+  case 22132:
+  case 22133:
+  case 23122:
+  case 23123:
+  case 23132:
+  case 23133:
+  case 31122:
+  case 31123:
+  case 31132:
+  case 31133:
+  case 32122:
+  case 32123:
+  case 32132:
+  case 32133:
+  case 33122:
+  case 33123:
+  case 33132:
+  case 33133: $type = $result[8][1]; $description = $result[8][2]; break;}
+?>
+
+<!doctype html>
+<html lang="">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!--Google Font-->
+<link href="" rel="stylesheet">
+<!-- Bootstrap CSS -->
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+<!--Font Awesome-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+<title>SURVEY</title>
+<!-- Custom styles for this template -->
+<link href="css/main.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div id="diagnosis">
+		<div class="container">
+			<div id="you_diagnosis" class="result_09">
+				<div id="logo"><a href="index.html"></a></div>
+				<div id="you_type">あなたは
+					<h2><?php echo $type; ?>
+<br>アイデアマンタイプ</h2>
+				</div>
+			</div>
+			<div id="you_personality" class="box">
+				<div class="ttl">あなたの基本性格</div>
+				<p><?php echo $description; ?></p>
+			</div>
+			<div class="btn_area">
+				<div class="btn"><a href="/index.html">もう一度診断する</a></div>
+			</div>
+		</div>
+	</div>
+<footer>
+	<a href=""></a>
+	<a href=""></a>
+	<p>Copyright (C) SkyLink all rights reserved.</p>
+</footer>
+</body>
+</html>
